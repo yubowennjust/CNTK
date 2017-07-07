90b2a660805dbdc421798e456a26bafa4e3fdf3d
REDMOND\sayanpa <sayanpa@microsoft.com>
2017-07-07 15:27:33 -0700

    FlappyBird using Keras-CNTK and DQN

---
+++ Tests/EndToEndTests/CNTKv2Python/Examples/FlappyBird_with_keras_DQN_test.py
@@ -0,0 +1,54 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+# Licensed under the MIT license. See LICENSE.md file in the project root
+# for full license information.
+# ==============================================================================
+
+import numpy as np
+import os
+import sys
+
+from cntk.device import try_set_default_device, gpu
+
+abs_path = os.path.dirname(os.path.abspath(__file__))
+example_dir = os.path.join(abs_path, "..", "..", "..", "..",
+                             "Examples", "ReinforcementLearning", 
+                             "FlappyBirdWithKeras")
+sys.path.append(example_dir)
+current_dir = os.getcwd()
+os.chdir(example_dir)
+
+# This example shows how the same keras model that runs on 
+# Tensorflow as posted at the github site of the author Ben Lau 
+# https://github.com/yanpanlau/Keras-FlappyBird) can be run
+# with CNTK API with minimum code change.
+# Note: please ignore the UserWarning the test emits due to a newer 
+# API for Convolution2D function available.
+ 
+def test_FlappyBird_with_keras_DQN_noerror(device_id):
+    from cntk.ops.tests.ops_test_utils import cntk_device
+    try_set_default_device(cntk_device(device_id))
+    
+    sys.path.append(example_dir)
+    current_dir = os.getcwd()
+    os.chdir(example_dir)
+    
+    import FlappyBird_with_keras_DQN as fbgame
+
+    model = fbgame.buildmodel()
+    args = {'mode': 'Run'}
+    res = fbgame.trainNetwork(model, args, internal_testing=True )
+    
+    np.testing.assert_array_equal(res, 0, err_msg='Error in running Flappy Bird example', verbose=True)
+    
+    args = {'mode': 'Train'}
+    res = fbgame.trainNetwork(model, args, internal_testing=True )
+    
+    np.testing.assert_array_equal(res, 0, err_msg='Error in testing Flappy Bird example', verbose=True)
+    
+    os.chdir(current_dir)
+    print("Done")
+
+
+
+
