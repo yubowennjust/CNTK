445a89e60420facd6b183de39e832ee1d745a129
REDMOND\sayanpa <sayanpa@microsoft.com>
2017-07-18 18:55:33 -0700

    Fixed jenkins error

--- Examples/ReinforcementLearning/FlappingBirdWithKeras/FlappingBird_with_keras_DQN.py
+++ Examples/ReinforcementLearning/FlappingBirdWithKeras/FlappingBird_with_keras_DQN.py
@@ -14,6 +14,8 @@ from skimage.transform import rotate
 from skimage.viewer import ImageViewer
 import sys
 
+os.environ["SDL_VIDEODRIVER"] = "dummy" 
+
 # Loat the right urlretrieve based on python version
 try: 
     from urllib.request import urlretrieve 
